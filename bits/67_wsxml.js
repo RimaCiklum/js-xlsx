a7a44c3fd02a4601aafe8620fe26df84fc245462
Pieter Sheth-Voss <pieter@protobi.com>
2015-12-04 09:49:11 -0500

    Add showGridlines

--- bits/67_wsxml.js
+++ bits/67_wsxml.js
@@ -520,8 +520,6 @@ function write_ws_xml(idx/*:number*/, opts, wb/*:Workbook*/, rels)/*:string*/ {
 
 	write_ws_xml_sheetpr(ws, wb, idx, opts, o);
 
-	o[o.length] = (writextag('dimension', null, {'ref': ref}));
-
 	o[o.length] = write_ws_xml_sheetviews(ws, opts, idx, wb);
 
 	/* TODO: store in WB, process styles */
@@ -533,6 +531,15 @@ function write_ws_xml(idx/*:number*/, opts, wb/*:Workbook*/, rels)/*:string*/ {
 
 	if(ws['!cols'] != null && ws['!cols'].length > 0) o[o.length] = (write_ws_xml_cols(ws, ws['!cols']));
 
+	o[o.length] = (writextag('dimension', null, {'ref': ref}));
+  var sheetView = writextag('sheetView', null,  {
+    showGridLines: opts.showGridLines == false ? '0' : '1',
+    tabSelected: opts.tabSelected === undefined ? '1' :  opts.tabSelected,
+    workbookViewId: opts.workbookViewId === undefined ? '0' : opts.workbookViewId
+  });
+  o[o.length] = writextag('sheetViews', sheetView);
+
+	if(ws['!cols'] !== undefined && ws['!cols'].length > 0) o[o.length] = (write_ws_xml_cols(ws, ws['!cols']));
 	o[sidx = o.length] = '<sheetData/>';
 	ws['!links'] = [];
 	if(ws['!ref'] != null) {
