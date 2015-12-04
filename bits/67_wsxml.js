f2bc97c4bfedb9f74bbb103ff79bee39d4139857
Pieter Sheth-Voss <pieter@protobi.com>
2015-12-04 09:53:43 -0500

    Add showGridLines as option

--- bits/67_wsxml.js
+++ bits/67_wsxml.js
@@ -539,6 +539,13 @@ function write_ws_xml(idx/*:number*/, opts, wb/*:Workbook*/, rels)/*:string*/ {
   });
   o[o.length] = writextag('sheetViews', sheetView);
 
+  var sheetView = writextag('sheetView', null,  {
+    showGridLines: opts.showGridLines == false ? '0' : '1',
+    tabSelected: opts.tabSelected === undefined ? '1' :  opts.tabSelected,
+    workbookViewId: opts.workbookViewId === undefined ? '0' : opts.workbookViewId
+  });
+  o[o.length] = writextag('sheetViews', sheetView);
+
 	if(ws['!cols'] !== undefined && ws['!cols'].length > 0) o[o.length] = (write_ws_xml_cols(ws, ws['!cols']));
 	o[sidx = o.length] = '<sheetData/>';
 	ws['!links'] = [];
